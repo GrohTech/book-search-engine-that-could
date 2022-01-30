38ff178cec92213f9dedf0a8d22ccb16fec7b0ba
Lauren Groh <legroh@uwm.edu>
2022-01-30 09:48:22 -0600

    try to fix authMiddleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const { ApolloServer } = require('apollo-server-express');
 // import typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 
+const { authMiddleware } = require('./utils/auth');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -16,14 +18,14 @@ const startServer = async () => {
   const server = new ApolloServer({ 
     typeDefs, 
     resolvers, 
-    // context: authMiddleware 
+    context: authMiddleware 
   });
-
-  // Start the Apollo server
-  await server.start();
 
   // integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
+
+  // Start the Apollo server
+  await server.start();
 
   // log where we can go to test our GQL API
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
@@ -41,6 +43,10 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+})
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
